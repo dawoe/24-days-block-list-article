f6e0e05e92678c7439a011abedad83b51924cf1b
Dave Woestenborghs <dave.woestenborghs@gmail.com>
2021-12-06 17:00:17 +0100

    Added styles sheet to master page

---
+++ 24Days.Core/Components/ClientAssetBundlingComponent.cs
@@ -0,0 +1,25 @@
+﻿using Umbraco.Cms.Core.Composing;
+using Umbraco.Cms.Core.WebAssets;
+
+namespace _24Days.Core.Components
+{
+    internal class ClientAssetBundlingComponent : IComponent
+    {
+        private readonly IRuntimeMinifier _runtimeMinifier;
+
+        public ClientAssetBundlingComponent(IRuntimeMinifier runtimeMinifier)
+        {
+            _runtimeMinifier = runtimeMinifier;
+        }
+
+        public void Initialize()
+        {
+            _runtimeMinifier.CreateCssBundle("css-bundle",
+                BundlingOptions.NotOptimizedAndComposite, "~/assets/styles.css");
+        }
+
+        public void Terminate()
+        {
+        }
+    }
+}
---
+++ 24Days.Core/Composers/ClientAssetBundlingComposer.cs
@@ -0,0 +1,9 @@
+﻿using _24Days.Core.Components;
+using Umbraco.Cms.Core.Composing;
+
+namespace _24Days.Core.Composers
+{
+    internal class ClientAssetBundlingComposer : ComponentComposer<ClientAssetBundlingComponent>
+    {
+    }
+}
