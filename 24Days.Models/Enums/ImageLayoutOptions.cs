8497b40b635e56775262b142c3297b78453cd585
Dave Woestenborghs <dave.woestenborghs@gmail.com>
2021-12-06 17:10:05 +0100

    Added image layout options datatype

---
+++ 24Days.Models/Enums/ImageLayoutOptions.cs
@@ -0,0 +1,13 @@
+﻿using Umbraco.Community.Contentment.DataEditors;
+
+namespace _24Days.Models.Enums
+{
+    public enum ImageLayoutOptions
+    {
+        [DataListItem(Name = "Left", Value = "left", Icon = "icon-navigation-left")]
+        Left = 0,
+
+        [DataListItem(Name = "Right", Value = "right", Icon = "icon-navigation-right")]
+        Right = 1,
+    }
+}
