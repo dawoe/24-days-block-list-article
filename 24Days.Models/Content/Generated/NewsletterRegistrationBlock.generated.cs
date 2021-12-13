218e7be9dc64eeeb9c91403beb97dae9879cda66
Dave Woestenborghs <dave.woestenborghs@gmail.com>
2021-12-13 21:02:15 +0100

    Added reusable content blocks

--- 24Days.Models/Content/Generated/NewsletterRegistrationBlock.generated.cs
+++ 24Days.Models/Content/Generated/NewsletterRegistrationBlock.generated.cs
@@ -18,9 +18,34 @@ using Umbraco.Extensions;
 
 namespace _Days.Models.Content
 {
+	// Mixin Content Type with alias "newsletterRegistrationBlock"
+	/// <summary>Newsletter Registration Block</summary>
+	public partial interface INewsletterRegistrationBlock : IPublishedElement
+	{
+		/// <summary>Button text</summary>
+		[global::System.CodeDom.Compiler.GeneratedCodeAttribute("Umbraco.ModelsBuilder.Embedded", "9.1.2+915f1cb34c3c63d3c88ebced61dd35d80255408c")]
+		[global::System.Diagnostics.CodeAnalysis.MaybeNull]
+		string ButtonText { get; }
+
+		/// <summary>Input field placeholder</summary>
+		[global::System.CodeDom.Compiler.GeneratedCodeAttribute("Umbraco.ModelsBuilder.Embedded", "9.1.2+915f1cb34c3c63d3c88ebced61dd35d80255408c")]
+		[global::System.Diagnostics.CodeAnalysis.MaybeNull]
+		string InputFieldPlaceholder { get; }
+
+		/// <summary>Tagline</summary>
+		[global::System.CodeDom.Compiler.GeneratedCodeAttribute("Umbraco.ModelsBuilder.Embedded", "9.1.2+915f1cb34c3c63d3c88ebced61dd35d80255408c")]
+		[global::System.Diagnostics.CodeAnalysis.MaybeNull]
+		string Tagline { get; }
+
+		/// <summary>Title</summary>
+		[global::System.CodeDom.Compiler.GeneratedCodeAttribute("Umbraco.ModelsBuilder.Embedded", "9.1.2+915f1cb34c3c63d3c88ebced61dd35d80255408c")]
+		[global::System.Diagnostics.CodeAnalysis.MaybeNull]
+		string Title { get; }
+	}
+
 	/// <summary>Newsletter Registration Block</summary>
 	[PublishedModel("newsletterRegistrationBlock")]
-	public partial class NewsletterRegistrationBlock : PublishedElementModel
+	public partial class NewsletterRegistrationBlock : PublishedElementModel, INewsletterRegistrationBlock
 	{
 		// helpers
 #pragma warning disable 0109 // new is redundant
@@ -55,7 +80,12 @@ namespace _Days.Models.Content
 		[global::System.CodeDom.Compiler.GeneratedCodeAttribute("Umbraco.ModelsBuilder.Embedded", "9.1.2+915f1cb34c3c63d3c88ebced61dd35d80255408c")]
 		[global::System.Diagnostics.CodeAnalysis.MaybeNull]
 		[ImplementPropertyType("buttonText")]
-		public virtual string ButtonText => this.Value<string>(_publishedValueFallback, "buttonText");
+		public virtual string ButtonText => GetButtonText(this, _publishedValueFallback);
+
+		/// <summary>Static getter for Button text</summary>
+		[global::System.CodeDom.Compiler.GeneratedCodeAttribute("Umbraco.ModelsBuilder.Embedded", "9.1.2+915f1cb34c3c63d3c88ebced61dd35d80255408c")]
+		[return: global::System.Diagnostics.CodeAnalysis.MaybeNull]
+		public static string GetButtonText(INewsletterRegistrationBlock that, IPublishedValueFallback publishedValueFallback) => that.Value<string>(publishedValueFallback, "buttonText");
 
 		///<summary>
 		/// Input field placeholder: Enter the optional placeholder text for the input field. If left empty the text "Email address" will be used
@@ -63,7 +93,12 @@ namespace _Days.Models.Content
 		[global::System.CodeDom.Compiler.GeneratedCodeAttribute("Umbraco.ModelsBuilder.Embedded", "9.1.2+915f1cb34c3c63d3c88ebced61dd35d80255408c")]
 		[global::System.Diagnostics.CodeAnalysis.MaybeNull]
 		[ImplementPropertyType("inputFieldPlaceholder")]
-		public virtual string InputFieldPlaceholder => this.Value<string>(_publishedValueFallback, "inputFieldPlaceholder");
+		public virtual string InputFieldPlaceholder => GetInputFieldPlaceholder(this, _publishedValueFallback);
+
+		/// <summary>Static getter for Input field placeholder</summary>
+		[global::System.CodeDom.Compiler.GeneratedCodeAttribute("Umbraco.ModelsBuilder.Embedded", "9.1.2+915f1cb34c3c63d3c88ebced61dd35d80255408c")]
+		[return: global::System.Diagnostics.CodeAnalysis.MaybeNull]
+		public static string GetInputFieldPlaceholder(INewsletterRegistrationBlock that, IPublishedValueFallback publishedValueFallback) => that.Value<string>(publishedValueFallback, "inputFieldPlaceholder");
 
 		///<summary>
 		/// Tagline: Enter an optional tagline that will appear above the title
@@ -71,7 +106,12 @@ namespace _Days.Models.Content
 		[global::System.CodeDom.Compiler.GeneratedCodeAttribute("Umbraco.ModelsBuilder.Embedded", "9.1.2+915f1cb34c3c63d3c88ebced61dd35d80255408c")]
 		[global::System.Diagnostics.CodeAnalysis.MaybeNull]
 		[ImplementPropertyType("tagline")]
-		public virtual string Tagline => this.Value<string>(_publishedValueFallback, "tagline");
+		public virtual string Tagline => GetTagline(this, _publishedValueFallback);
+
+		/// <summary>Static getter for Tagline</summary>
+		[global::System.CodeDom.Compiler.GeneratedCodeAttribute("Umbraco.ModelsBuilder.Embedded", "9.1.2+915f1cb34c3c63d3c88ebced61dd35d80255408c")]
+		[return: global::System.Diagnostics.CodeAnalysis.MaybeNull]
+		public static string GetTagline(INewsletterRegistrationBlock that, IPublishedValueFallback publishedValueFallback) => that.Value<string>(publishedValueFallback, "tagline");
 
 		///<summary>
 		/// Title: Enter the title for the block
@@ -79,6 +119,11 @@ namespace _Days.Models.Content
 		[global::System.CodeDom.Compiler.GeneratedCodeAttribute("Umbraco.ModelsBuilder.Embedded", "9.1.2+915f1cb34c3c63d3c88ebced61dd35d80255408c")]
 		[global::System.Diagnostics.CodeAnalysis.MaybeNull]
 		[ImplementPropertyType("title")]
-		public virtual string Title => this.Value<string>(_publishedValueFallback, "title");
+		public virtual string Title => GetTitle(this, _publishedValueFallback);
+
+		/// <summary>Static getter for Title</summary>
+		[global::System.CodeDom.Compiler.GeneratedCodeAttribute("Umbraco.ModelsBuilder.Embedded", "9.1.2+915f1cb34c3c63d3c88ebced61dd35d80255408c")]
+		[return: global::System.Diagnostics.CodeAnalysis.MaybeNull]
+		public static string GetTitle(INewsletterRegistrationBlock that, IPublishedValueFallback publishedValueFallback) => that.Value<string>(publishedValueFallback, "title");
 	}
 }
